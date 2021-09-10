bd07a924953e9fce9e0bfa803dd8f0a66f219832
Benjamin Munoz <b.munoz@uniandes.edu.co>
2021-09-10 16:29:37 -0500

    printsortresults

--- App/view.py
+++ App/view.py
@@ -88,6 +88,16 @@ def printSortResults(ord_books, sample=10):
     # TODO completar modificaciones para el laboratorio 4
     pass
 
+def printSortResults(ord_books, sample=10):
+    size = lt.size(ord_books)
+    if size > sample:
+        print("Los primeros ", sample, " libros ordenados son:") 
+        i=1
+        while i <= sample:
+            book = lt.getElement(ord_books,i)
+            print('Titulo: ' + book['title'] + ' ISBN: ' +
+            book['isbn'] + ' Rating: ' + book['average_rating'])
+            i+=1
 catalog = None
 
 """
@@ -127,7 +137,14 @@ while True:
         result = controller.sortBooks(catalog, int(size))
         print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
                                           str(result))
+    
+    elif int(inputs[0]) == 5:
+        size = input("Indique tamaño de la muestra: ")
+        result = controller.sortBooks(catalog, int(size))
+        print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ", str(result[0]))
+        printSortResults(result[1])
 
     else:
         sys.exit(0)
 sys.exit(0)
+
